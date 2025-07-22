a9f9255718dafc2c3c2970848ea8da3147595df3
MiguelViHe <miguelvidalhernando@gmail.com>
2025-07-22 10:41:38 +0200

    module_01/ex02 terminado

---
+++ module_01/ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvidal-h <mvidal-h@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 09:50:29 by mvidal-h          #+#    #+#             */
+/*   Updated: 2025/07/22 10:27:13 by mvidal-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str; //PUNTERO: puede cambiar la referencia a quien apunta. como los punteros en C.
+	std::string	&stringREF = str; //REFERENCIA: exactamente como la variable. puedes cambiar el contenidoy cambiará en la variable original. NO PUEDE cambiar la referencia de la variable. se quedará siempre apuntando a la misma.
+
+	std::cout << "Memory address of the string variable: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Value of the string variable: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
