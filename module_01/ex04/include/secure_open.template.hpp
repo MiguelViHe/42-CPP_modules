2f0d4e7a611ee8d75c9eef400de3d0a0d89b0b7e
MiguelViHe <miguelvidalhernando@gmail.com>
2025-07-23 11:37:44 +0200

    module_01/ex04 terminado

---
+++ module_01/ex04/include/secure_open.template.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   secure_open.template.hpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvidal-h <mvidal-h@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 16:25:18 by mvidal-h          #+#    #+#             */
+/*   Updated: 2025/07/23 10:31:43 by mvidal-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SECURE_OPEN_HPP
+#define SECURE_OPEN_HPP
+
+#include <fstream>
+#include <iostream>
+
+template <typename Stream>
+int secure_open(const std::string& filename, Stream& stream)
+{
+	stream.open(filename.c_str());
+	if (!stream) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
+#endif
---
+++ module_01/ex04/src/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvidal-h <mvidal-h@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 15:42:25 by mvidal-h          #+#    #+#             */
+/*   Updated: 2025/07/23 11:26:28 by mvidal-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "secure_open.template.hpp"
+
+int	ft_replace(std::string& line, const std::string& s1, const std::string& s2)
+{
+	std::size_t pos;
+	while (true)
+	{
+		pos = line.find(s1);
+		if (pos == std::string::npos)
+			break;
+		line.erase(pos, s1.length());
+		line.insert(pos, s2);
+	}
+	return (0);
+}
+
+int	main(int argc, const char **argv)
+{
+	std::ifstream	infile;
+	std::ofstream	outfile;
+	std::string		line;
+
+	if (argc == 4)
+	{
+		if (secure_open(std::string(argv[1]), infile) != 0) //std::ifstream infile(argv[1]); Esto al hacerlo en la declaración ya se encarga de abrirlo directamente.
+			return (1);
+		if (secure_open(std::string(argv[1]) + ".replace", outfile) != 0)
+			return (1);
+		while (std::getline(infile, line))
+		{
+			ft_replace(line, std::string(argv[2]), std::string(argv[3]));
+			outfile << line << std::endl;
+		}
+		infile.close();
+		outfile.close();
+	}
+	else {
+		std::cerr << "Uso: " << argv[0] << " <file> <s1> <s2>" << std::endl;
+		return (1);
+	}
+}
