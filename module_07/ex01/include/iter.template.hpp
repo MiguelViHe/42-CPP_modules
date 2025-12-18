6c9f7f746e428a4f4ba12861157b028626266b12
MiguelViHe <miguelvidalhernando@gmail.com>
2025-12-18 12:16:33 +0100

    module07/ex01 puesta la segunda template para ni entervenir el en tipo con const

--- module_07/ex01/include/iter.template.hpp
+++ module_07/ex01/include/iter.template.hpp
@@ -5,7 +5,17 @@
 
 template <typename T, typename F>
 void	iter(T* array, const size_t len, F func) {
-	if (!array || len == 0 || !func)
+	if (!array || len <= 0)
+		return ;
+	for (size_t i = 0; i < len; i++)
+	{
+		func(array[i]);
+	}
+}
+
+template <typename T, typename F>
+void	iter(T const* array, const size_t len, F func) {
+	if (!array || len <= 0)
 		return ;
 	for (size_t i = 0; i < len; i++)
 	{
